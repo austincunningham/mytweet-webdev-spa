de032e7054498df9a16726e96566f779807082de
Austin Cunningham <austincunningham@oceanfree.net>
2018-09-12 23:27:13 +0100

    point the base url at the heroku app

--- src/services/fixtures.js
+++ src/services/fixtures.js
@@ -2,6 +2,7 @@
  * Created by austin on 28/12/2016.
  */
 export default class Fixtures {
-  baseUrl = 'http://localhost:4000';
+  //baseUrl = 'http://localhost:4000';
   //baseUrl = 'http://35.160.157.123:4000';
+  baseUrl = 'https://mytweet-web-austin.herokuapp.com';
 }
